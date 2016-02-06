52913a79c78305ba91ead3561d55424c556d5ed0
George Lo <twgeolo@gmail.com>
2016-02-06 14:26:06 -0500

    Added scraper. Working not yet extensively tested.

--- backend/README.md
+++ backend/README.md
@@ -3,8 +3,9 @@
 ##Install
 You should have Python 2.7.X installed (All Mac OS X machines and some Linux machines comes with this pre-installed)
 The flask framework can be installed through Python Package Manager using ```pip install flask; pip install flask-cors```
+The requests framework can be installed through Python Package Manager using ```pip install requests```
 
-##Running 
+##Running
 Start the server with ```python server.py```
 The server will now be bound to port 5000 on your localhost
 
@@ -13,6 +14,36 @@ The default pages that exist are accessed at '/' and '/api'. The first is a test
 
 ##Usage
 TODO
-##Example 
+
+##Example
 To send a move request through your browser ```curl -i -H "Content-Type: application/json" -X POST -d '{"uri":"https://www.youtube.com/watch?v=IuysY1BekOE"}' http://localhost:5000/api/1/remove```
 
+##Scraper
+###YouTube
+Endpoint: /api/search/youtube
+Request Type: Get
+Parameters:
+| Key | Value                                                                      |
+|-----|----------------------------------------------------------------------------|
+| q   | Required. The search string.                                               |
+| d   | Optional. Debug mode: 'true' or 'false'. If not set, the default is false. |
+Response:
+| Key         | Value                                                      |
+|-------------|------------------------------------------------------------|
+| status      | 'success' or 'failure'                                     |
+| description | When status is failure, it will include the error.         |
+| items       | When status is success, it will include an array of media. |
+###SoundCloud
+Endpoint: /api/search/soundcloud
+Request Type: Get
+Parameters:
+| Key | Value                                                                      |
+|-----|----------------------------------------------------------------------------|
+| q   | Required. The search string.                                               |
+| d   | Optional. Debug mode: 'true' or 'false'. If not set, the default is false. |
+Response:
+| Key         | Value                                                      |
+|-------------|------------------------------------------------------------|
+| status      | 'success' or 'failure'                                     |
+| description | When status is failure, it will include the error.         |
+| items       | When status is success, it will include an array of media. |
--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, url_for
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -44,7 +46,55 @@ def removeMedia(stationid):
 	#TODO: Parse JSON object and remove it for the station's queue if exists
 	return jsonify({'status':'success'}), 201
 
+@app.route('/api/search/youtube', methods=['GET'])
+def searchYouTube():
+	q = request.args.get('q')
+	if not q:
+		return jsonify({'status':'failure', 'description':'missing argument \'q\''}), 201
+	response = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=relevance&q=' + q + '&type=video&videoDefinition=any&key=AIzaSyASsdIg2A2eb-W9UlfOx4sIVRLZ2y1h63c')
+	json_obj = response.json()
+	items = json_obj['items']
+	d = request.args.get('debug')
+	if d and d != 'true' and d != 'false':
+		return jsonify({'status':'failure', 'description':'argument \'d\' should be \'true\' or \'false\''}), 201
+	if not d or d == 'false':
+		for item in items:
+			for key, value in item['snippet'].items():
+				item[key] = value
+			item['videoId'] = item['id']['videoId']
+			if item['thumbnails']['high']:
+				item['thumbnailUrl'] = item['thumbnails']['high']['url']
+			elif item['thumbnails']['medium']:
+				item['thumbnailUrl'] = item['thumbnails']['medium']['url']
+			else:
+				item['thumbnailUrl'] = item['thumbnails']['default']['url']
+			item['videoUrl'] = 'https://www.youtube.com/watch?v=' + item['videoId']
+			del item['etag'], item['kind'], item['id'], item['snippet'], item['channelId'], item['liveBroadcastContent'], item['thumbnails']
+	return jsonify({'status':'success', 'items':items}), 201 
 
+@app.route('/api/search/soundcloud', methods=['GET'])
+def searchSoundCloud():
+	q = request.args.get('q')
+	if not q:
+		return jsonify({'status':'failure', 'description':'missing argument \'q\''}), 201
+	response = requests.get('http://api.soundcloud.com/tracks.json?client_id=87dd0b14dbc81970b9b4becdf176c498&q=' + q + '&limit=50')
+	json_obj = response.json()
+	d = request.args.get('debug')
+	if d and d != 'true' and d != 'false':
+		return jsonify({'status':'failure', 'description':'argument \'d\' should be \'true\' or \'false\''}), 201
+	if not d or d == 'false':
+		for item in json_obj:
+			item['thumbnail'] = item['artwork_url']
+			item['videoUrl'] = item['video_url']
+			item['waveformUrl'] = item['waveform_url']
+			item['trackUrl'] = item['permalink_url']
+			item['playCount'] = item['playback_count']
+			item['lastModified'] = item['last_modified']
+			item['trackId'] = item['id']
+			item['format'] = item['original_format']
+			item['publishedAt'] = item['created_at']
+			del item['user'], item['user_favorite'], item['user_id'], item['user_playback_count'], item['video_url'], item['waveform_url'], item['state'], item['stream_url'], item['streamable'], item['tag_list'], item['track_type'], item['uri'], item['sharing'], item['reposts_count'], item['release_year'], item['release_month'], item['release_day'], item['release'], item['purchase_url'], item['purchase_title'], item['permalink'], item['permalink_url'], item['playback_count'], item['license'], item['likes_count'], item['last_modified'], item['label_name'], item['label_id'], item['kind'], item['key_signature'], item['isrc'], item['id'], item['original_format'], item['original_content_size'], item['genre'], item['favoritings_count'], item['embeddable_by'], item['downloadable'], item['download_url'], item['download_count'], item['created_at'], item['commentable'], item['comment_count'], item['bpm'], item['attachments_uri'], item['artwork_url']
+	return jsonify({'status':'success', 'items':json_obj}), 201
 
 if __name__ == '__main__':
 	app.run(debug=True)
