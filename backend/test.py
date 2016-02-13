2b4fdc3a61e211cd6e80771f904c546cea1c8a2b
George Lo <twgeolo@gmail.com>
2016-02-12 20:08:54 -0500

    Added tests for soundcloud & fixed a typo in WebSocket server code

--- backend/test.py
+++ backend/test.py
@@ -24,5 +24,23 @@ class CrawlerTestCase(unittest.TestCase):
 		self.assertEquals(json_data['status'], 'failure')
 		self.assertEquals(json_data['description'], 'argument \'d\' should be \'true\' or \'false\'')
 		
+		response = test_app.get('/api/search/soundcloud')
+		json_data = json.loads(response.data)
+		self.assertEquals(response.status, '201 CREATED')
+		self.assertEquals(json_data['status'], 'failure')
+		self.assertEquals(json_data['description'], 'missing argument \'q\'')
+		
+		response = test_app.get('/api/search/soundcloud', query_string='q=iceskating+trex')
+		json_data = json.loads(response.data)
+		self.assertEquals(response.status, '201 CREATED')
+		self.assertEquals(json_data['status'], 'success')
+		self.assertIsNotNone(json_data['items'])
+		
+		response = test_app.get('/api/search/soundcloud', query_string='q=iceskating+trex&debug=afdsaf')
+		json_data = json.loads(response.data)
+		self.assertEquals(response.status, '201 CREATED')
+		self.assertEquals(json_data['status'], 'failure')
+		self.assertEquals(json_data['description'], 'argument \'d\' should be \'true\' or \'false\'')
+		
 if __name__ == '__main__':
 	unittest.main()
