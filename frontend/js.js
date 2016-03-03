65d5b85315fa51ab5b475b9342153deaf96d71be
Mitch Holm <mitch@Mitchs-MBP.lfytina2.metronetinc.com>
2016-03-03 14:25:44 -0500

    added a ton of features

--- frontend/js.js
+++ frontend/js.js
@@ -1,22 +1,49 @@
 var username;
+var ownerName;
+var searchQueueYT;
+var searchQueueSC;
+var stationNum;
+var socket;
+
+var adjectives = ["angry","quick","fast","cute","adorable","tiny","ferocious","ugly","smart","dumb","long","spooky","moon"]
+
+
+var animals = ["Lion","Tiger","Bear","Snake","Fox","Cat","Dog","Jackalope","Rabbit","Tim"];
 
 $(document).ready(function (){
 	username = getCookie('username')
 	while(username === "")
-		username = prompt("Enter a username","Guest" + Math.floor(Math.random() * 1000000));
+		username = prompt("Enter a username", generateUsername());
 	document.cookie = 'username=' + username;
 	$("#user").text(username);
 })
 
-$('#user').on("click", function(e){
-	var tusername = prompt("Enter a username","Guest" + Math.floor(Math.random() * 1000000));
-	if(tusername != "")
-		username = tusername;
-	else
-		username = "Guest" + Math.floor(Math.random() * 1000000);
-	document.cookie = 'username=' + username;
-	$("#user").text(username);
-});
+function generateUsername(){
+	return adjectives[Math.floor(Math.random() * adjectives.length)] + animals[Math.floor(Math.random() * animals.length)]
+}
+
+function addbox(num){
+	if(stationNum == null){
+		return;
+	}
+	if(num < 5){
+		$.post("http://localhost:2000/api/" + stationNum + "/add",searchQueueYT[num],function(data){
+				addToPlayBar(searchQueueYT[num]);
+		})
+	} else {
+		$.post("http://localhost:2000/api/" + stationNum + "/add",searchQueueSC[num-5],function(data){
+				addToPlayBar(searchQueueSC[num-5]);
+		})
+	}
+
+}
+
+// $('#user').on("click", function(e){
+
+// 	username = generateUsername();
+// 	document.cookie = 'username=' + username;
+// 	$("#user").text(username);
+// });
 
 
 //Lifted from W3Schools
@@ -37,16 +64,19 @@ $('#searchBox').on("keypress", function (e) {
 
         // Cancel the default action on keypress event
         e.preventDefault();
+        emptySearchWindow();
 
-        $.get("http://localhost:5000/api/search/youtube",{q:$('#searchBox').val()}, function (data){
-        		emptySearchWindow();
+        $.get("http://localhost:2000/api/search/youtube",{q:$('#searchBox').val()}, function (data){
+        	console.log(data)
+        		searchQueueYT = data.items;
         		for(var i = 0; i < 5; i++){
-        			createYTSearchPreviewItem(data.items[i])
+        			createYTSearchPreviewItem(data.items[i],i)
         		}
         	}, "json");
-        $.get("http://localhost:5000/api/search/soundcloud",{q:$('#searchBox').val()}, function (data){
+        $.get("http://localhost:2000/api/search/soundcloud",{q:$('#searchBox').val()}, function (data){
+        		searchQueueSC = data.items;
         		for(var i = 0; i < 5; i++){
-        			createSCSearchPreviewItem(data.items[i])
+        			createSCSearchPreviewItem(data.items[i],i)
         		}
         	}, "json");
     }
@@ -56,32 +86,47 @@ function emptySearchWindow(){
 	$('#searchWindow').empty();
 }
 
-function createYTSearchPreviewItem(datum){
-	var preview = "<div class='previewBox'><img src='" + datum.thumbnailUrl + "' /><h1>" + datum.title + "</h1><p>" + datum.channelTitle + "</p></div>";
+function createYTSearchPreviewItem(datum,num){
+	var preview = "<a onclick='addbox(" + num + ")'><div class='previewBox'><img src='" + datum.thumbnailUrl + "' /><h1>" + datum.title + "</h1><p>" + datum.channelTitle + "</p></div></a>";
+
+
 	$('#searchWindow').append(preview)
 }
 
-function createSCSearchPreviewItem(datum){
+function createSCSearchPreviewItem(datum, num){
 	if(datum.thumbnail == null)
 		datum.thumbnail = "soundcloud.png";
 	if(datum.description == null || datum.description === "")
 		datum.description = "Not Provided"
-	var preview = "<div class='previewBox'><img src='" + datum.thumbnail + "'/><h1>" + datum.title + "</h1><p>" + datum.uploader + "</p></div>";
+	var preview = "<a onclick='addbox(" + (num + 5) + ")'><div class='previewBox'><img src='" + datum.thumbnail + "'/><h1>" + datum.title + "</h1><p>" + datum.uploader + "</p></div></a>";
 	$('#searchWindow').append(preview);
 }
 
+function addToPlayBar(item, num){
+	var preview = "<div class='playlistBox' onclick='remove()'><img src='" + item.thumbnailUrl + "' /><h1>" + item.title + "</h1><p>" + item.channelTitle + "</p></div>";
+	$('#leftBar').append(preview);
+}
+
 function loadStation(data){
-	clearPlaylist();
-	$.each(data.queue, function(item){
-		addToPlayList(item);
-	})
-	stationView();
-	setStationColor(data.color);
+	$.get("http://localhost:2000/api/" + data, null, function(data){
+		//receive array of all the media objects
+		setStationColor(data.hex);
+		ownerName = data.owner;
+		stationView();
+		$('#leftBar').empty();
+		var num = 0;
+		$.each(data.queue, function(item){
+			addToPlayBar(item);
+		})
+
+		console.log(data);
+	});
 }
 
 function setStationColor(color){
-	$('#header').css('background-color', color);
-	$('#footer').css('background-color', "0px 3px " + color)
+	console.log('testing');
+	$('header').css('background-color', color);
+	$('footer').css('background-color', color)
 }
 
 function stationView(){
@@ -90,15 +135,19 @@ function stationView(){
 	$('#stations').hide();
 	$('#content').show();
 	$('#stationCreator').hide();
+	socket = new WebSocket("ws://localhost:5000");
 }
 
 function browseView(){
 	loadStationList();
+	ownerName = null;
+	stationNum = null;
 	$('#leftBar').hide();
 	$('#chatBar').hide();
 	$('#stations').show();
 	$('#content').hide();
 	$('#stationCreator').hide();
+	socket.close();
 }
 
 function loadStationList(){
@@ -120,15 +169,16 @@ $('#create').on("click", function(e) {
 	if(stationName === "" || stationColor === "")
 		return;
 
-	$.post("http://localhost:5000/api/create", {
+	$.post("http://localhost:2000/api/create", {
 		title: stationName,
 		color: stationColor,
 		owner: username,
 		visible: $("#stationVisible").val()
 	}, function(data) {
-		loadStation(data)
+		// console.log(data)
+		stationNum = data.stationId;
+		loadStation(data.stationId)
 	}, "json");
-	console.log("Failed to create station");
 })
 
 $('#refreshStations').on("click", function(e){
@@ -137,4 +187,13 @@ $('#refreshStations').on("click", function(e){
 
 $('#logo').on("click", function(e) {
 	browseView();
+})
+
+$('#chatBarInput').on("keypress", function(e){
+	if(e.keyCode == 13 && $('#chatBarInput').val() !== ""){
+		$('#chatArea').prepend("<p>" + $('#chatBarInput').val() + "</p>");
+		$('#chatBarInput').val("");
+
+
+	}
 })
